038d3b0e1cc90e3acdfdef896f2f9fcc515391f6
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-10-30 08:38:34 +0100

    chore: extend ESLint rules

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import { flatConfigs as importX } from 'eslint-plugin-import-x'
+import js from '@eslint/js'
+import markdown from '@eslint/markdown'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default defineConfig([
+  globalIgnores([
+    'dist/*',
+    'spec/*',
+  ]),
+  {
+    files: ['**/*.js'],
+    languageOptions: { ecmaVersion: 'latest', globals: { ...globals.browser, L: 'readonly' } },
+    plugins: { js, stylistic },
+    extends: [importX.recommended, 'js/recommended', 'stylistic/recommended'],
+    rules: {
+      'no-var': 'error',
+    },
+  },
+  { files: ['**/*.md'], plugins: { markdown }, language: 'markdown/gfm', extends: ['markdown/recommended'] },
+])
